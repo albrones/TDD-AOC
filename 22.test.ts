711cb0aced7ff13ae9187f228753a14b0c714c09
Alexis B <abrones1@gmail.com>
2025-02-07 12:15:29 +0100

    Create deck constructor with factory and reverse method

--- 22.test.ts
+++ 22.test.ts
@@ -4,3 +4,33 @@ import { Deck } from './22.js';
 test('warpup', () => {
   expect(true).toBe(true);
 });
+test('should have a deck factory order (Top 0 1 2 3 4 5 6 7 8 9 Bottom)', () => {
+  //Arrange
+  let deck;
+  //Act
+  deck = new Deck(10);
+  //Assert
+  expect(deck.value.length).toEqual(10);
+  expect([...deck.value]).toEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
+});
+
+test('should depile the deck (Top 0 1 2 3 4 5 6 7 8 9 Bottom) and have a deck at reverse order at the end', () => {
+  //Arrange
+  let deck = new Deck(10);
+  let expectedDeck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //Act
+  deck.reverse();
+  //Assert
+  expect([...deck.value]).toEqual([...expectedDeck]);
+});
+
+test('factory deck should have 57 cards ', () => {
+  //Arrange
+  let deck;
+  //Act
+  deck = new Deck(57);
+  //Assert
+  expect(deck.value[deck.value.length - 1]).toEqual(0);
+  expect(deck.value[0]).toEqual(56);
+  expect(deck.value.length).toEqual(57);
+});
--- 22.ts
+++ 22.ts
@@ -1,2 +1,12 @@
 export class Deck {
+  value: number[];
+  constructor(deckSize: number) {
+    this.value = this.initFactoryDeck(deckSize);
+  }
+  initFactoryDeck(deckSize: number) {
+    return Array.from(Array(deckSize).keys()).reverse();
+  }
+  reverse() {
+    return this.value.reverse();
+  }
 }
